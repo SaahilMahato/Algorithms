5c96d8ba3938b085ec57ff76e89bb4ce3b8f102b
SaahilMahato <saahilmahato72@gmail.com>
2020-07-23 18:06:10 +0545

    Added InsertionSort Python

---
+++ InsertionSort/python/InsertionSort.py
@@ -0,0 +1,26 @@
+class InsertionSort:
+    def __init_(self):
+        self.arr = []
+
+    def input(self):
+        self.arr = list(map(int, input("Enter the array: ").split()))
+
+    def sort(self):
+        for i in range(1, len(self.arr)):
+            temp = self.arr[i]
+            empty_index = i
+            while empty_index > 0  and self.arr[empty_index-1] > temp:
+                self.arr[empty_index] = self.arr[empty_index-1]
+                empty_index -= 1
+            self.arr[empty_index] = temp
+
+    def print_array(self):
+        print("Array is: " + " ".join([str(i) for i in self.arr]))
+
+
+if __name__ == '__main__':
+    s = InsertionSort()
+    s.input()
+    s.print_array()
+    s.sort()
+    s.print_array()
