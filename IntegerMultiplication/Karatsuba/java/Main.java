b78366a004c61f5fa424c5aa4f4b15bb4015242d
SaahilMahato <saahilmahato72@gmail.com>
2020-07-30 23:00:05 +0545

    closed scanner

--- IntegerMultiplication/Karatsuba/java/Main.java
+++ IntegerMultiplication/Karatsuba/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         long x = sc.nextLong();
         System.out.print("Enter y: ");
         long y = sc.nextLong();
+        sc.close();
         Karatsuba k = new Karatsuba();
         System.out.println(k.multiply(x, y));
     }
