1df9e319450939f0fc11e9fd397f0e255411aada
SaahilMahato <saahilmahato72@gmail.com>
2020-07-30 08:40:16 +0545

    Karatsuba java

---
+++ IntegerMultiplication/Karatsuba/java/Karatsuba.java
@@ -0,0 +1,29 @@
+import java.lang.Math;
+import java.lang.Math;
+
+class Karatsuba {
+    public long multiply(long x, long y) {
+        if(x < 10 || y < 10) {
+            return x * y;
+        }
+        else {
+            int len_x = String.valueOf(x).length();
+            int len_y = String.valueOf(y).length();
+            int n = Math.max(len_x, len_y);
+            int m = n / 2;
+
+            long a = x / (long)Math.pow(10, m);
+            long b = x % (long)Math.pow(10, m);
+            long c = y / (long)Math.pow(10, m);
+            long d = y % (long)Math.pow(10, m);
+
+            long ac = multiply(a, c);
+            long bd = multiply(b, d);
+            long sum_ad_bc = multiply(a+b, c+d) - ac - bd;
+            
+            long result = ac * (long)Math.pow(10, 2*m) + (sum_ad_bc * (long)Math.pow(10, m)) + bd;
+
+            return result;
+        }
+    }
+}
---
+++ IntegerMultiplication/Karatsuba/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x: ");
+        long x = sc.nextLong();
+        System.out.print("Enter y: ");
+        long y = sc.nextLong();
+        Karatsuba k = new Karatsuba();
+        System.out.println(k.multiply(x, y));
+    }
+}
